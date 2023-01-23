c86a8abebe3bea7c383a66732080d3a5636639d9
marcolg26 <62360013+marcolg26@users.noreply.github.com>
2023-01-23 19:55:34 +0100

    Update course.py

--- pages/course.py
+++ pages/course.py
@@ -1,33 +1,17 @@
 import streamlit as st
 import backend as be
 
-<<<<<<< HEAD
-st.set_page_config(layout="wide")
-
-course_id = st.experimental_get_query_params()
-
-if len(course_id) == 0:
-    st.header("Couldn't find this course, maybe it has been deleted")
-else:
-    course_id = float(course_id["cid"][0])
-
-course=be.getcourseinfo(course_id)
-st.header(course['title'].iloc[0])
-
-comments=be.getcoursecomm(course_id)
-=======
 arg = st.experimental_get_query_params()
 
 course_ID=int(arg["u"][0])
 
 course=be.getcourseinfo(course_ID)
 comments=be.getcoursecomm(course_ID)
->>>>>>> e5bab6d14d373fefd28ccbc52486dc3193dd6e12
 
 if(course.size==0): st.subheader("ID not found")
 else:
     st.header(course['title'].iloc[0])
-    
+
     if(comments.size==0): st.subheader("No comments :(")
     else:
         st.subheader("Comments ("+str(len(comments))+")")
