e1784d4bdfcc245c8ab4d4710d34a789e9e33bbd
marcolg26 <62360013+marcolg26@users.noreply.github.com>
2023-01-23 19:55:55 +0100

    author courses

--- pages/author.py
+++ pages/author.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import backend as be
 
 st.set_page_config(layout="wide")
 
@@ -11,3 +11,10 @@ if len(instructor_url) == 0:
     st.header("Couldn't find this instructor, maybe his profile has been deleted")
 else:
     st.header(instructor_url["u"][0])
+
+    courses=be.getauthorcourses(instructor_url["u"][0])
+
+    i=0
+    for course in courses.iterrows():
+        st.write(courses['title'].iloc[i])
+        i=i+1
