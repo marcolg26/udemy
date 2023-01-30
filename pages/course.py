1ea13d732323e3250d0bcdaf58168ec1b6db424a
marcolg26 <62360013+marcolg26@users.noreply.github.com>
2023-01-30 15:19:38 +0100

    trend chart

--- pages/course.py
+++ pages/course.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import backend as be
+import altair as alt
 
 be.style()
 
 arg = st.experimental_get_query_params()
-st.experimental_set_query_params()
+#st.experimental_set_query_params()
 
 if len(arg) == 0 or "cid" not in arg:
     st.header("Couldn't find this course, maybe it has been deleted")
@@ -17,12 +18,23 @@ else:
     if course.size == 0:
         st.subheader("ID not found")
     else:
-        st.header(course['title'].iloc[0])
+        st.title(course['title'].iloc[0])
+
+        st.header("Trend")
+
+
+        chart=alt.Chart(comments[comments['course_id'] == course_ID]).mark_line().encode(
+            x='year(date):T',
+            y='count()')
+        chart.encoding.x.title = 'month'
+        chart.encoding.y.title = 'new comments'
+
+        st.altair_chart(chart, use_container_width=True)
 
         if (comments.size == 0):
             st.subheader("No comments :(")
         else:
-            st.subheader("Comments ("+str(len(comments))+")")
+            st.header("Top comments ("+str(len(comments))+")")
             i = 0
             for comment in comments.iterrows():
                 st.write(str(comments['display_name'].iloc[i])+":")
