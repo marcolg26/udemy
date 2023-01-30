734da4fe7b084fc042f7a3625a396502cb7b183b
marcolg26 <62360013+marcolg26@users.noreply.github.com>
2023-01-30 13:41:19 +0100

    prices pie chart

--- pages/courses_summary.py
+++ pages/courses_summary.py
@@ -65,6 +65,25 @@ with col1:
     )
 
     st.altair_chart(pie, use_container_width=True)
+
+with col2:
+    st.subheader("Prices")
+
+    price = pd.DataFrame({"price": ['Free', '<10', '<20', '<50', '<100', '100+'], "value": 0})
+    
+    price.loc[price['price'] == 'Free', ['value']]= len(coursesdb[coursesdb['price'] ==0])
+    price.loc[price['price'] == '<10', ['value']]=  len(coursesdb[coursesdb['price'].between(1, 10)])
+    price.loc[price['price'] == '<20', ['value']]=  len(coursesdb[coursesdb['price'].between(10, 20)])
+    price.loc[price['price'] == '<50', ['value']]=  len(coursesdb[coursesdb['price'].between(20, 50)])
+    price.loc[price['price'] == '<100', ['value']]= len(coursesdb[coursesdb['price'].between(50, 100)])
+    price.loc[price['price'] == '100+', ['value']]= len(coursesdb[coursesdb['price'] >100])
+
+    pie= alt.Chart(price).mark_arc(innerRadius=50).encode(
+    theta=alt.Theta(field="value", type="quantitative"),
+    color=alt.Color(field="price", type="nominal"),
+    )
+
+    st.altair_chart(pie, use_container_width=True)
     
 
 
