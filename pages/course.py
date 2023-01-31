e0a13b086316b5b81b405e004ec82af69c23f409
AlessandroViol <viol.alessandro97@gmail.com>
2023-01-31 11:29:54 +0100

    Introduced topic modeling with PCA to extract keywords from comments

--- pages/course.py
+++ pages/course.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import backend as be
 import altair as alt
+import pandas as pd
+from sklearn.decomposition import PCA #pip install -U scikit-learn
 import string
 import nltk #pip install nltk
 from nltk.corpus import stopwords #python        import nltk         nltk.download("stopwords")
@@ -57,7 +59,6 @@ else:
             stop_words = set(stopwords.words(course.language))
             lemmatizer = WordNetLemmatizer()
             #st.write(stop_words)
-            comments["tokens"] = None
 
             tf_matrix = {}
             doc_count_matrix = {}
@@ -109,8 +110,37 @@ else:
             comments["score"] = None
             i=0
             for key, comment in tf_idf_matrix.items():
-                comments["score"].iloc[i] = sum(comment.values())/len(comment)
+                if len(comment) > 0: 
+                    comments["score"].iloc[i] = sum(comment.values())/len(comment)
                 i = i + 1
+
+            df_tf_idf = pd.DataFrame(tf_idf_matrix).T.fillna(0)
+            #st.write(df_tf_idf)
+
+            pca = PCA(n_components=3, random_state=16)
+
+            df_pca = pd.DataFrame(
+                data=pca.fit_transform(df_tf_idf),
+                columns=[f"topic_{n}" for n in range(pca.n_components_)]
+            )
+
+            #st.write(df_pca)
+
+            df_weights = pd.DataFrame(
+                data=pca.components_,
+                columns=idf_matrix.keys(),
+                index=[f"topic_{n}" for n in range(pca.n_components_)]
+            )
+
+            df_weights = df_weights.T
+            #st.write(df_weights)
+            col1, col2, col3 = st.columns(3)
+            with col1: 
+                st.write(df_weights.sort_values("topic_0", ascending=False))
+            with col2: 
+                st.write(df_weights.sort_values("topic_1", ascending=False))
+            with col3: 
+                st.write(df_weights.sort_values("topic_2", ascending=False))
 
             st.header("Top comments")
             threshold = 1.3*comments["score"].mean()
