c3e8f5190c7b3d0ff08ee5cb60f00f99c9b13c98
marcolg26 <62360013+marcolg26@users.noreply.github.com>
2023-02-03 19:52:18 +0100

    improved suggested courses

--- backend.py
+++ backend.py
@@ -329,7 +329,7 @@ def get_courses():
     elif st.session_state.order == "Publishing date":
         list.sort_values(by='published_time', inplace=True, ascending=False)
     elif st.session_state.order == "Suggested ✨":
-        list['avg_rating_round'] = list['avg_rating'].round(decimals=1)
+        list['avg_rating_round'] = list['avg_rating'].round(decimals=0)
         list.sort_values(['avg_rating_round', 'num_comments', 'num_subscribers'],
                          ascending=[False, False, False], inplace=True)
 
